605000d153785223a8519ab218cab8145a553b33
Noah Laux <noahlaux@gmail.com>
2022-12-09 16:24:26 +0100

    Update commitlintJiraConstants.ts

--- packages/commitlint-jira-utils/src/commitlintJiraConstants.ts
+++ packages/commitlint-jira-utils/src/commitlintJiraConstants.ts
@@ -12,10 +12,6 @@ export const TASK_ID_SEPARATOR = '-'
 export const UPPERCASE = 'uppercase'
 export const LOWERCASE = 'lowercase'
 export const COMMIT_DESCRIPTION_SEPARATOR = '\n'
-// Adapted from rules at https://support.atlassian.com/bitbucket-cloud/docs/use-smart-commits/
-// Smart Commits only support the default Jira Software issue key format.
-// This format is two or more uppercase letters, followed by a hyphen and the issue number,
-// for example JRA - 123.
 
 /**
  * Adapted from rules at https://support.atlassian.com/bitbucket-cloud/docs/use-smart-commits/
